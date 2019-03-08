89f141fbb0b2aca252ed5bc64ee733285d210517
Ronan Fegan <rfegan13@gmail.com>
2019-03-08 19:36:55 +0000

    added function to sort list

--- researchPart1.js
+++ researchPart1.js
@@ -6,6 +6,29 @@ var pushNumber = function(number){
   listOfNumbers.push(number);
 }
 
+
+
+
+//Export another function that returns a version of the data list that is sorted in ascending order. The function you use to sort the data correctly should not be accessible from outside the module. (Think back to the Custom Sorting Exercise when handling sorting)
+
+
+
+pushNumber(1);
+pushNumber(4);
+pushNumber(10);
+pushNumber(3);
+
+
+
+let sortedList = listOfNumbers.sort(function(a,b){
+    return a - b
+})
+
+let returnSortFunction = function(){
+  return sortedList;
+}
+
+
+
 exports.pushNumber = pushNumber;
-
-
+exports.returnSortFunction = returnSortFunction;
